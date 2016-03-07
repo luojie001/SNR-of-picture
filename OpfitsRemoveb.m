d59f6f3106f41a27174a4e9d8724f3e0c482baee
zuotongren <wlj8593@qq.com>
2016-03-07 16:35:33 +0800

    Update OpfitsRemoveb.m

--- OpfitsRemoveb.m
+++ OpfitsRemoveb.m
@@ -1,23 +1,23 @@
  function [img] =OpfitsRemoveb( file_path)
-%º¯ÊıÔËĞĞĞèÒªÒ»·Ö¶àÖÓ£¬È¥±³¾°ºóµÄÍ¼Æ¬¿ÉÒÔµ÷ÓÃ
+%å‡½æ•°è¿è¡Œéœ€è¦ä¸€åˆ†å¤šé’Ÿï¼Œå»èƒŒæ™¯åçš„å›¾ç‰‡å¯ä»¥è°ƒç”¨
 img_path_list=dir(fullfile(file_path,'*.fit'));
-%»ñÈ¡¸ÃÎÄ¼ş¼ĞÖĞËùÓĞµÄfit¸ñÊ½µÄÍ¼Ïñ
+%è·å–è¯¥æ–‡ä»¶å¤¹ä¸­æ‰€æœ‰çš„fitæ ¼å¼çš„å›¾åƒ
 img_num=length(img_path_list);
-%»ñÈ¡Í¼Ïñ×ÜÊıÁ¿
-if img_num>0 %Âú×ãÌõ¼şµÄÍ¼Ïñ
-img=zeros(1024,1024,450);
+%è·å–å›¾åƒæ€»æ•°é‡
+if img_num>0 %æ»¡è¶³æ¡ä»¶çš„å›¾åƒ
+img=zeros(1024,1024,450); %å®šä¹‰ä¸‰ç»´æ•°ç»„
 for j=1:img_num
-image_name=img_path_list(j).name;%Í¼ÏñÃû
+image_name=img_path_list(j).name;%å›¾åƒå
 image=fitsread(fullfile(file_path,image_name));
-E_a=mean(image(:));%ÆÚÍû
-D_a=std2(image);%¾ù·½²î
+E_a=mean(image(:));%æœŸæœ›
+D_a=std2(image);%å‡æ–¹å·®
 s=ones(1024)*(E_a+D_a);
 s1=image-s;
 s2=abs(s1);
 img(:,:,j)=(s1+s2)*0.5;
 
 %fprintf('%d %d %s\n',i,j,strcat(file_path,image_name));
-%ÏÔÊ¾ÕıÔÚ´¦ÀíµÄÍ¼ÏñÃû
+%æ˜¾ç¤ºæ­£åœ¨å¤„ç†çš„å›¾åƒå
 end
 end
 end
