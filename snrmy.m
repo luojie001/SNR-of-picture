c2bde06c4104fe124235b2893b4d22be610f69f4
luojie001 <wlj8593@qq.com>
2016-03-13 12:20:16 +0800

    选取区域可以设定

--- snrmy.m
+++ snrmy.m
@@ -1,10 +1,11 @@
 function [snr] =snrmy(f)
-S=zeros(3,3);%取3*3的模板
+x=8;%局部区域所选的大小
+S=zeros(x+1,x+1);%取3*3的模板
 [m,n]=size(f);
-ph=zeros(m-2,n-2);%用ph来存放 所求的std
-for k=1:m-2
-    for l=1:n-2
-    S=f(k:k+2,l:l+2);
+ph=zeros(m-x,n-x);%用ph来存放 所求的std
+for k=1:m-x
+    for l=1:n-x
+    S=f(k:k+x,l:l+x);
      ph(k,l)=std(S(:));
     end
 end
