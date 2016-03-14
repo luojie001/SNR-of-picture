202437451db748f45e1be6a6835c438b10e2b84b
luojie001 <wlj8593@qq.com>
2016-03-14 11:21:22 +0800

    最亮点在的区域的信噪比
    速度有点慢

--- snrofallmax/maxfind.m
+++ snrofallmax/maxfind.m
@@ -1,4 +1,5 @@
-f=fitsread('E:\FITS\20140421_180444797.fit');
+function [ imagem] = maxfind(f)
+%寻找最大值所在的区域
 E_a=mean(f(:));%期望
 D_a=std2(f);%均方差
 %*******去背景********
@@ -6,7 +7,6 @@ s=ones(1024)*(E_a+D_a);
 s1=f-s;
 s2=abs(s1);
 image=(s1+s2)*0.5;%去背景后的图片 
-E_i=mean(image(:));%真实图片的期望
 [xi,yi]=find(image==max(max(image)));%最大值所在点的坐标
 n=0;s=0;w=0;e=0;
 while (image(xi-n,yi))>0
@@ -21,4 +21,6 @@ end
 while (image(xi,yi+e))>0
     e=e+1;
 end
-imagem=image(xi-n:xi+s,yi-w:yi+e);%选取的区域
+imagem=f(xi-n:xi+s,yi-w:yi+e);%选取的区域
+end
+
--- snrofallmax/snrmy.m
+++ snrofallmax/snrmy.m
@@ -1,12 +1,12 @@
 function [snr] =snrmy(f)
+%求信噪比
 x=8;%局部区域所选的大小
-S=zeros(x+1,x+1);%取3*3的模板
 [m,n]=size(f);
 ph=zeros(m-x,n-x);%用ph来存放 所求的std
 for k=1:m-x
     for l=1:n-x
-    S=f(k:k+x,l:l+x);
-     ph(k,l)=std(S(:));
+    s=f(k:k+x,l:l+x);
+     ph(k,l)=std(s(:));
     end
 end
 phmax=max(max(ph));
